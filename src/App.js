44d65a215a5c883729744eb0ae37c4af47c69711
nbrandon62 <nbrandon62@gmail.com>
2022-05-09 23:26:22 -0700

    removed useEffect and added route param

--- src/App.js
+++ src/App.js
@@ -6,28 +6,13 @@ import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
-
-  const [products, setProductData] = useState([]);
-
-    async function productData(){
-      const url = 'https://fakestoreapi.com/products';
-      const response = await fetch(url);
-      const data = await response.json();
-
-      setProductData(data);
-  }
-   
-  useEffect (()=> {
-    productData();
-  }, [])
-
   return (
       <>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/productpage" element={<ProductPage products={products} />} />
-        <Route path="/productpage/:id" element={<SingleProductPage products={products} />} />
+        <Route path="/productpage" element={<ProductPage />} />
+        <Route path="/productpage/:id" element={<SingleProductPage />} />
         <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
       <Footer />
