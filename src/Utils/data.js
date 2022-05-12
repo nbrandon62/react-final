51b5fd5f4dde4c9a44d81a14122c0cd22c2efa2c
nbrandon62 <nbrandon62@gmail.com>
2022-05-11 17:48:14 -0700

    tried fixing image bug, still broken

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -1,26 +1,25 @@
-import React from "react"
-import laptop from './Images/laptop.jpg'
-import jewlery from './Images/jewlery.jpg'
-import fashion from './Images/fashion.webp'
+import React from "react";
+import laptop from "./Images/laptop.jpg";
+import jewlery from "./Images/jewlery.jpg";
+import fashion from "./Images/fashion.webp";
 
 export const jumboItems = [
-    {
-        id: 1,
-        title: "GET THE LATEST COMPUTERS",
-        description: "Supplying the world with the newest gaming technologies",
-        img: <img src={laptop} />,
-    },
-    {
-        id: 2,
-        title: "SHOP JEWELRY FOR LOW PRICES",
-        description: "Up to 40% off select pieces!",
-        img: <img src={jewlery} />,
-    },
-    {
-        id: 3,
-        title: "NEW SPRING ARRIVALS",
-        description: "shop men or women for the latest fashion",
-        img: <img src={fashion} />,
-    }
-
-]
+  {
+    id: 1,
+    title: "GET THE LATEST COMPUTERS",
+    description: "Supplying the world with the newest gaming technologies",
+    img: <img src={laptop} />,
+  },
+  {
+    id: 2,
+    title: "SHOP JEWELRY FOR LOW PRICES",
+    description: "Up to 40% off select pieces!",
+    img: <img src={jewlery} />,
+  },
+  {
+    id: 3,
+    title: "NEW SPRING ARRIVALS",
+    description: "shop men or women for the latest fashion",
+    img: <img src={fashion} />,
+  },
+];
