be2e4aeb78e2ddb33b264d05e11a4cacbdbc9f4a
nbrandon62 <nbrandon62@gmail.com>
2022-05-11 17:48:03 -0700

    tried fixing image bug, still broken

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -1,120 +1,115 @@
-import React from 'react'
-import styled from 'styled-components'
-import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
-import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
-import {jumboItems} from "../Utils/data"; 
-import { useState } from 'react';
+import React from "react";
+import styled from "styled-components";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
+import { jumboItems } from "../Utils/data";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const Container = styled.div`
-    width: 100%;
-    height:100vh;
-    display: flex;
-    position: relative;
-    overflow: hidden;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px; 
-    background-color: transparent;
-    cursor: pointer;
-    position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 0;
-    bottom: 0;
-    left: ${props=> props.direction === "left" && "20px"};
-    right: ${props=> props.direction === "right" && "20px"};
-    margin: auto;
-    z-index: 2; 
-`
+  width: 50px;
+  height: 50px;
+  background-color: transparent;
+  cursor: pointer;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "20px"};
+  right: ${(props) => props.direction === "right" && "20px"};
+  margin: auto;
+  z-index: 2;
+`;
 const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    transform: translateX(${props=> props.jumboIndex * -100}vw);
-    transition: all 1s ease;
-`
+  height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.jumboIndex * -100}vw);
+  transition: all 1s ease;
+`;
 const Slide = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
 const ImageContainer = styled.div`
-    height: 100%;
-    flex: 1;
-    margin-left: 5rem;
-
-`
+  height: 400px;
+  width: 400px;
+  flex: 1;
+  margin-left: 5rem;
+  object-fit: cover;
+`;
 const Image = styled.img`
-    height: 90%;
-    width: 100%;
-`
+  object-fit: contain;
+`;
 const InfoContainer = styled.div`
-    flex: 1;
-    margin-left: 8rem;  
-`
+  flex: 1;
+  margin-left: 8rem;
+`;
 const Title = styled.h1`
-    font-size: 70px;
-`
-const Description = styled.p `
-    font-size: 20px ;
-    margin: 50px 0px;
-    letter-spacing: 1px;
-`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  font-size: 20px;
+  margin: 50px 0px;
+  letter-spacing: 1px;
+`;
 const Button = styled.button`
-    padding: 10px;
-    font-size: 20px;
-    background-color: transparent;
-    cursor: pointer;
-`
-
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Jumbotron = () => {
-
-    const [jumboIndex, setJumboIndex] = useState(0);
-    const handleClick = (direction) => {
-        if(direction === "left"){
-            setJumboIndex(jumboIndex > 0 ? jumboIndex -1 : 2); 
-        } else{
-            setJumboIndex(jumboIndex < 2 ? jumboIndex +1 : 0);
-        }
-    };    
-
+  const [jumboIndex, setJumboIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setJumboIndex(jumboIndex > 0 ? jumboIndex - 1 : 2);
+    } else {
+      setJumboIndex(jumboIndex < 2 ? jumboIndex + 1 : 0);
+    }
+  };
 
   return (
     <Container>
-        <Arrow direction = "left" onClick = {() => handleClick("left")}>
-            <ArrowCircleLeftOutlinedIcon /> 
-        </Arrow>
-         
-        {/* This is the first slide in the jumbotron */}
-        <Wrapper jumboIndex = { jumboIndex }>
-            {jumboItems.map(item=>(
-              <Slide>
-                <ImageContainer> 
-                    <Image src={item.img}/> 
-                </ImageContainer>
-                <InfoContainer>
-                    <Title>{item.title}</Title>
-                    <Description>{item.description}</Description>
-                    <Link to="/productpage" >
-                        <Button>SHOP NOW</Button>
-                    </Link>
-                </InfoContainer>
-              </Slide>
-            ))}  
-        </Wrapper>
-        
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowCircleLeftOutlinedIcon />
+      </Arrow>
 
-        <Arrow direction = "right" onClick = {() => handleClick("right")}>
-            <ArrowCircleRightOutlinedIcon /> 
-        </Arrow>
+      {/* This is the first slide in the jumbotron */}
+      <Wrapper jumboIndex={jumboIndex}>
+        {jumboItems.map((item) => (
+          <Slide>
+            <ImageContainer>
+              <Image src={item.img} />
+            </ImageContainer>
+
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>{item.description}</Description>
+              <Link to="/productpage">
+                <Button>SHOP NOW</Button>
+              </Link>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowCircleRightOutlinedIcon />
+      </Arrow>
     </Container>
+  );
+};
 
-    )
-}
-
-export default Jumbotron
+export default Jumbotron;
